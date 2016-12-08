541b3ae237fa5bd24d81346c1a7c5b3a083199a6
yannan <yannan786@gmail.com>
2016-12-08 23:50:31 +0800

    user

--- project/flymStyle/common/js/topBar.js
+++ project/flymStyle/common/js/topBar.js
@@ -12,10 +12,12 @@
     $('body').on('click', '.icon-close', function() {
         var _src = $(this).parents('li')[0].attributes['data-src'].textContent;
         $(this).parents('li').remove();
-        var parentLi = $(parent.document).find('.sidebar-third a');
+        var parentLi = $(parent.document).find('.sidebar a');
         for(i=0;i<parentLi.length;i++) {
-            if(parentLi[i].attributes['data-src'].textContent == _src) {
-                parentLi[i].attributes['data-exist'].textContent = 'false';
+            if(parentLi[i].attributes['data-src']) {
+                if(parentLi[i].attributes['data-src'].textContent == _src) {
+                    parentLi[i].attributes['data-exist'].textContent = 'false';
+                }
             }
         }
         $('#contentModel')[0].src = 'content.html';
@@ -25,10 +27,12 @@
     $('body').on('dblclick', '.top-menu-ul li', function() {
         var _src = $(this).attr('data-src');
         $(this).remove();
-        var parentLi = $(parent.document).find('.sidebar-third a');
+        var parentLi = $(parent.document).find('.sidebar a');
         for(i=0;i<parentLi.length;i++) {
-            if(parentLi[i].attributes['data-src'].textContent == _src) {
-                parentLi[i].attributes['data-exist'].textContent = 'false';
+            if(parentLi[i].attributes['data-src']) {
+                if(parentLi[i].attributes['data-src'].textContent == _src) {
+                    parentLi[i].attributes['data-exist'].textContent = 'false';
+                }
             }
         }
         $('#contentModel')[0].src = 'content.html';
@@ -41,6 +45,7 @@
         $('.top-logo').removeClass('current');
         $(this).addClass('current').siblings().removeClass('current');
         $('#contentModel')[0].src = $(this).attr('data-src');
+        labelScroll();
     });
 
 //    统计时间标签选择
@@ -56,6 +61,50 @@
         $('#contentModel')[0].src = 'content.html';
     })
 
+
+    //标签列表长度
+    function getlabelLength() {
+        var topulLength = 0;
+        $('.top-menu-ul li').each(function (i) {
+            topulLength += ($(this).width() + 2);
+        });
+        return topulLength;
+    }
+
+    //标签列表超长显示/隐藏
+    function labelScroll() {
+        var containerWidth = $('.top-bar').width() - $('.top-logo').width();
+        $('.top-menu').width(containerWidth);
+        $('.top-menu-ul').width(getlabelLength());
+
+        if(containerWidth < getlabelLength()) {
+            $('.menu-ctr').unbind('click');
+            $('.menu-ctr').show();
+        } else {
+            $('.menu-ctr').unbind('click');
+            $('.menu-ctr').hide();
+            $('.top-menu-ul').animate({left: 0});
+        }
+
+    }
+
+    $('body').on('click', '.menu-ctr-left', function () {
+        var _left = parseInt($('.top-menu-ul').css("left")) - 200 + "px";
+        console.log(_left);
+        $('.top-menu-ul').animate({left: _left});
+    });
+    $('body').on('click', '.menu-ctr-right', function () {
+        var _left = parseInt($('.top-menu-ul').css("left")) + 200 + "px";
+        if(parseInt($('.top-menu-ul').css("left")) < 0 ) {
+            $('.top-menu-ul').animate({left: _left});
+        }
+
+    });
+    console.log($('body'));
+
+    $(window).resize(function () {
+        labelScroll();
+    })
 
     //select 美化
     $('select').select2({
